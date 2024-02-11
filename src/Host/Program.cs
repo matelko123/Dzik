c6023c96fb606cfbe06602cd0ff66ceeabb4e995
Mateusz <mateusz280798@gmail.com>
2024-02-11 14:17:17 +0100

    FIX: Docker compose db connection

--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -10,13 +10,13 @@ builder.Host.UseSerilog((context, configuration) =>
     configuration.ReadFrom.Configuration(context.Configuration));
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddTransient<ExceptionHandlingMiddleware>();
+
 
 var app = builder.Build();
 
