fa492411d2e68c8336c36b646324238088144e61
Mateusz <mateusz280798@gmail.com>
2024-02-17 22:28:01 +0100

    ADD: Seed roles with claims to db.

--- src/Infrastructure/Persistence/Initialization/ApplicationDbSeeder.cs
+++ src/Infrastructure/Persistence/Initialization/ApplicationDbSeeder.cs
@@ -1,7 +1,10 @@
 using Domain.Entities.Identity;
 using Infrastructure.Persistence.Context;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Shared.Authorization.Constants.Permission;
+using Shared.Authorization.Constants.Role;
 
 namespace Infrastructure.Persistence.Initialization;
 
@@ -27,9 +30,52 @@ internal sealed class ApplicationDbSeeder
         await SeedAdminUserAsync();
     }
     
-    private Task SeedRolesAsync(AppDbContext dbContext)
+    private async Task SeedRolesAsync(AppDbContext dbContext)
     {
-        return Task.CompletedTask;
+        foreach (string roleName in RoleConstants.DefaultRoles)
+        {
+            if (await _roleManager.Roles.SingleOrDefaultAsync(r => r.Name == roleName)
+                is not AppRole role)
+            {
+                // Create the role
+                _logger.LogInformation("Seeding {role} Role", roleName);
+                role = new AppRole(roleName, $"{roleName} Role.");
+                await _roleManager.CreateAsync(role);
+            }
+
+            // Assign permissions
+            if (roleName == RoleConstants.BasicRole)
+            {
+                await AssignPermissionsToRoleAsync(dbContext, FSHPermissions.Basic, role);
+            }
+            else if (roleName == RoleConstants.AdministratorRole)
+            {
+                await AssignPermissionsToRoleAsync(dbContext, FSHPermissions.Admin, role);
+                await AssignPermissionsToRoleAsync(dbContext, FSHPermissions.Root, role);
+            }
+        }
+    }
+
+    private async Task AssignPermissionsToRoleAsync(AppDbContext dbContext, IReadOnlyList<FSHPermission> permissions, AppRole role)
+    {
+        var currentClaims = await _roleManager.GetClaimsAsync(role);
+        foreach (var permission in permissions)
+        {
+            if (!currentClaims.Any(c => c.Type == ApplicationClaimTypes.Permission && c.Value == permission.Name))
+            {
+                _logger.LogInformation("Seeding {role} Permission '{permission}'", role.Name, permission.Name);
+                dbContext.RoleClaims.Add(new AppRoleClaim
+                {
+                    RoleId = role.Id,
+                    ClaimType = ApplicationClaimTypes.Permission,
+                    ClaimValue = permission.Name,
+                    CreatedBy = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                    Group = permission.Resource,
+                    Description = permission.Description,
+                });
+                await dbContext.SaveChangesAsync();
+            }
+        }
     }
 
     private Task SeedAdminUserAsync()
