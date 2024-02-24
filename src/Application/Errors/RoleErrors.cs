cbc6b6cdeba46f83558b2595a1e862b848ff0613
Mateusz <mateusz280798@gmail.com>
2024-02-24 12:18:43 +0100

    ADD: Role errors

--- src/Application/Errors/RoleErrors.cs
+++ src/Application/Errors/RoleErrors.cs
@@ -1,9 +1,13 @@
+using Contracts.Common;
+using System.Net;
+
 namespace Application.Errors;
 
 public static class RoleErrors
 {
-    public const string Failed = "Register role failed";
-    public const string NotFound = "Role not found.";
-    public const string NotAllowed = "Not allowed to modify Role.";
-    public const string NotAllowedBeingUsed = "Not allowed to delete Role as it is being used.";
+    public static Error Failed => new(HttpStatusCode.BadRequest, "Register role failed.");
+    public static Error AlreadyExists => new(HttpStatusCode.BadRequest, "Role already exists.");
+    public static Error NotAllowed => new(HttpStatusCode.Unauthorized, "Not allowed to modify Role.");
+    public static Error NotAllowedBeingUsed => new(HttpStatusCode.UnprocessableContent, "Not allowed to delete Role as it is being used.");
+    public static Error NotFound => new(HttpStatusCode.NotFound, "Role not found.");
 }
