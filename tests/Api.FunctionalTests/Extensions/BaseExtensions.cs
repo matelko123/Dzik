6bcf9ff5cd1af3cc697d363cccd6622f999e263a
Mateusz <mateusz280798@gmail.com>
2024-03-04 21:48:48 +0100

    ADD: Extension methods to tests

---
+++ tests/Api.FunctionalTests/Extensions/BaseExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Api.FunctionalTests.Extensions;
+
+internal static class BaseExtensions
+{
+    private static readonly Random Random = new();
+    internal static string RandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[Random.Next(s.Length)]).ToArray());
+    }
+
+    internal static string RandomNumeric(int length = 9)
+        => Random.Next(1 * 10 ^ length, 9 * 10 ^ length).ToString();
+}
