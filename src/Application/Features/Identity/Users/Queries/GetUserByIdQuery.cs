aeef47ae901a6a886e0e29c05f73a4e7a6a9f915
Mateusz <mateusz280798@gmail.com>
2024-03-04 21:46:15 +0100

    REF: GetUserByIdQuery

--- src/Application/Features/Identity/Users/Queries/GetUserByIdQuery.cs
+++ src/Application/Features/Identity/Users/Queries/GetUserByIdQuery.cs
@@ -1,31 +1,29 @@
 using Application.Abstractions.Messaging;
 using Application.Errors;
+using Contracts.Identity.Authentication;
 using Domain.Entities.Identity;
+using Mapster;
 using Microsoft.AspNetCore.Identity;
 using Shared.Wrapper;
 
 namespace Application.Features.Identity.Users.Queries;
 
-public sealed record GetUserByIdQuery(Guid UserId) : ICachedQuery<Result<AppUser>>
+public sealed record GetUserByIdQuery(Guid UserId) 
+    : ICachedQuery<Result<UserDto>>
 {
     public string Key => $"users-by-id-{UserId}";
-    public TimeSpan? Expiration => null;
+    public TimeSpan? Expiration => TimeSpan.FromMinutes(5);
 }
 
-internal sealed class GetUserByIdQueryHandler : IQueryHandler<GetUserByIdQuery, Result<AppUser>>
+internal sealed class GetUserByIdQueryHandler(
+    UserManager<AppUser> userManager) 
+    : IQueryHandler<GetUserByIdQuery, Result<UserDto>>
 {
-    private readonly UserManager<AppUser> _userManager;
-
-    public GetUserByIdQueryHandler(UserManager<AppUser> userManager)
+    public async Task<Result<UserDto>> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
     {
-        _userManager = userManager;
-    }
-
-    public async Task<Result<AppUser>> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
-    {
-        AppUser? user = await _userManager.FindByIdAsync(request.UserId.ToString());
+        AppUser? user = await userManager.FindByIdAsync(request.UserId.ToString());
         return user is null 
-            ? Result<AppUser>.Error(UserErrors.NotFound) 
-            : Result<AppUser>.Success(user);
+            ? Result<UserDto>.Error(UserErrors.NotFound) 
+            : Result<UserDto>.Success(user.Adapt<UserDto>());
     }
 }
