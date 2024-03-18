2907aa61f572df4f1f2409b010a13190399573a0
Mateusz <mateusz280798@gmail.com>
2024-03-18 20:19:27 +0100

    ADD: GetUsersByRoleAsync

--- src/Infrastructure/Identity/UserService.cs
+++ src/Infrastructure/Identity/UserService.cs
@@ -1,5 +1,8 @@
+using Application.Errors;
 using Application.Identity.Users;
+using Contracts.Identity.Authentication;
 using Domain.Entities.Identity;
+using Infrastructure.Persistence.Context;
 using Mapster;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -7,21 +10,30 @@ using Shared.Wrapper;
 
 namespace Infrastructure.Identity;
 
-public class UserService : IUserService
+public class UserService(
+    UserManager<AppUser> userManager, 
+    AppDbContext appDbContext) 
+    : IUserService
 {
-    private readonly UserManager<AppUser> _userManager;
-
-    public UserService(UserManager<AppUser> userManager)
-    {
-        _userManager = userManager;
-    }
 
     public async Task<bool> ExistsWithNameAsync(string name)
-        => await _userManager.FindByNameAsync(name) is not null;
+        => await userManager.FindByNameAsync(name) is not null;
 
     public async Task<bool> ExistsWithEmailAsync(string email, Guid? exceptId = null)
-        => await _userManager.FindByEmailAsync(email) is { } user && user.Id != exceptId;
+        => await userManager.FindByEmailAsync(email) is { } user && user.Id != exceptId;
 
     public async Task<bool> ExistsWithPhoneNumberAsync(string phoneNumber, Guid? exceptId = null)
-        => await _userManager.Users.FirstOrDefaultAsync(x => x.PhoneNumber == phoneNumber) is { } user && user.Id != exceptId;
+        => await userManager.Users.FirstOrDefaultAsync(x => x.PhoneNumber == phoneNumber) is { } user && user.Id != exceptId;
+
+    public async Task<Result<List<UserDto>>> GetUsersByRoleAsync(Guid roleId, CancellationToken cancellationToken = default)
+    {
+        var role = await appDbContext.Roles.SingleOrDefaultAsync(x => x.Id == roleId, cancellationToken);
+        if (role is null)
+        {
+            return Result<List<UserDto>>.Error(RoleErrors.NotFound);
+        }
+
+        var users = await userManager.GetUsersInRoleAsync(role.Name!);
+        return Result<List<UserDto>>.Success(users.Adapt<List<UserDto>>());
+    }
 }
