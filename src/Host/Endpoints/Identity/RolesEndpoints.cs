9d2fa69d3586cd3267fff6ef533f244f41362dc0
Mateusz <mateusz280798@gmail.com>
2024-03-18 20:17:31 +0100

    REF: Add api route consts and refactor endpoints

--- src/Host/Endpoints/Identity/RolesEndpoints.cs
+++ src/Host/Endpoints/Identity/RolesEndpoints.cs
@@ -1,9 +1,12 @@
 ﻿using Application.Identity.Roles;
+using Application.Identity.Users;
 using Contracts.Common;
 using Contracts.Identity.Roles;
 using Domain.Entities.Identity;
 using Host.Endpoints.Internal;
 using Host.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Shared.ApiEndpoints;
 using Shared.Wrapper;
 
 namespace Host.Endpoints.Identity;
@@ -13,11 +16,11 @@ public class RolesEndpoints : IEndpoints
     public static void DefineEndpoints(IEndpointRouteBuilder app)
     {
         var rolesGroup = app
-            .MapGroup("api/roles")
+            .MapGroup(RolesEndpointsConstants.Base)
             .WithTags("Roles");
 
-        rolesGroup.MapGet("/", async (
-            IRoleService roleService, CancellationToken cancellationToken) =>
+        rolesGroup.MapGet(RolesEndpointsConstants.GetAllRoles, async (
+            [FromServices] IRoleService roleService, CancellationToken cancellationToken) =>
         {
             var roles = await roleService.GetListAsync(cancellationToken);
             return roles.ToApiResult();
@@ -32,35 +35,45 @@ public class RolesEndpoints : IEndpoints
             .Produces<Result<List<RoleDto>>>();
 
 
-        rolesGroup.MapGet("/{id:guid}", async (
-            Guid id,
-            IRoleService roleService, CancellationToken cancellationToken) =>
+        rolesGroup.MapGet(RolesEndpointsConstants.GetRoleById, async (
+            [FromRoute] Guid id,
+            [FromServices] IRoleService roleService, CancellationToken cancellationToken) =>
         {
-            var role = await roleService.GetByIdWithPermissionsAsync(id, cancellationToken);
+            var role = await roleService.GetByIdAsync(id, cancellationToken);
             return role.ToApiResult();
         })
             .WithName("GetRoleById")
             .Produces<Result<RoleDto>>()
             .Produces<ErrorResult>(StatusCodes.Status500InternalServerError);
 
+        rolesGroup.MapGet(RolesEndpointsConstants.GetUsersByRoleId, async (
+            [FromRoute] Guid id,
+            [FromServices] IUserService userService, CancellationToken cancellationToken) =>
+        {
+            var role = await userService.GetUsersByRoleAsync(id, cancellationToken);
+            return TypedResults.Ok(role);
+        })
+            .WithName("GetUsersByRoleId")
+            .Produces<Result<RoleDto>>()
+            .Produces<ErrorResult>(StatusCodes.Status500InternalServerError);
 
-        rolesGroup.MapPost("/", async (
-            CreateRoleRequest request,
-            IRoleService roleService, CancellationToken cancellationToken) =>
+        rolesGroup.MapPost(RolesEndpointsConstants.CreateRole, async (
+            [FromBody] RoleDto request,
+            [FromServices] IRoleService roleService, CancellationToken cancellationToken) =>
         {
             var role = new AppRole(request.Name, request.Description);
             var result = await roleService.CreateAsync(role, cancellationToken);
             return result.ToApiResult();
         })
             .WithName("CreateRole")
-            .Accepts<CreateRoleRequest>(EndpointConstants.ContentType)
+            .Accepts<RoleDto>(EndpointConstants.ContentType)
             .Produces<Result>(StatusCodes.Status201Created)
             .Produces<ErrorResult>(StatusCodes.Status500InternalServerError);
 
 
-        rolesGroup.MapDelete("/{id:guid}", async (
-            Guid id,
-            IRoleService roleService, CancellationToken cancellationToken) =>
+        rolesGroup.MapDelete(RolesEndpointsConstants.DeleteRole, async (
+            [FromRoute] Guid id,
+            [FromServices] IRoleService roleService, CancellationToken cancellationToken) =>
         {
             var result = await roleService.DeleteAsync(id, cancellationToken);
             return result.ToApiResult();
@@ -70,14 +83,15 @@ public class RolesEndpoints : IEndpoints
             .Produces<ErrorResult>(StatusCodes.Status500InternalServerError);
 
 
-        rolesGroup.MapPut("/{id:guid}", async (
-            Guid id, UpdateRoleRequest request,
-            IRoleService roleService, CancellationToken cancellationToken) =>
+        rolesGroup.MapPut(RolesEndpointsConstants.UpdateRole, async (
+            [FromRoute] Guid id, [FromBody] RoleDto request,
+            [FromServices] IRoleService roleService, CancellationToken cancellationToken) =>
         {
-            var roles = await roleService.UpdateAsync(request with { RoleId = id }, cancellationToken);
+            var roles = await roleService.UpdateAsync(id, request, cancellationToken);
             return roles.ToApiResult();
         })
             .WithName("UpdateRole")
+            .Accepts<RoleDto>(EndpointConstants.ContentType)
             .Produces<Result>()
             .Produces<ErrorResult>(StatusCodes.Status500InternalServerError);
     }
---
+++ src/Shared/ApiEndpoints/RolesEndpointsConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.ApiEndpoints;
+
+public static class RolesEndpointsConstants
+{
+    public const string Base = "api/identity/roles";
+
+    public const string GetAllRoles = "/";
+    public const string GetRoleById = $"/{{id}}";
+    public const string GetUsersByRoleId = $"/{{id}}/users";
+    public const string CreateRole = "/";
+    public const string DeleteRole = $"/{{id}}";
+    public const string UpdateRole = $"/{{id}}";
+}
